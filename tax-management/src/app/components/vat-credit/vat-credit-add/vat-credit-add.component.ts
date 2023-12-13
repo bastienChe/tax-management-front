9acb2fa5fea669e99f8583a64d6f12198a9ce5e7
Bastien Chevriaut <bastien.chevriaut-ext@pole-emploi.fr>
2023-12-13 14:20:20 +0100

    creating form

--- tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
+++ tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
@@ -1,40 +1,40 @@
 import { Component } from '@angular/core';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {MatIconModule} from "@angular/material/icon";
+import {BrowserModule} from "@angular/platform-browser";
+import {WebcamImage, WebcamInitError, WebcamModule} from "ngx-webcam";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 
 @Component({
   selector: 'app-vat-credit-add',
   standalone: true,
-  imports: [MatIconModule, HttpClientModule],
+  imports: [MatIconModule, HttpClientModule, FormsModule, WebcamModule, CommonModule],
   templateUrl: './vat-credit-add.component.html',
   styleUrl: './vat-credit-add.component.css'
 })
 export class VatCreditAddComponent {
 
+  capturedImage: WebcamImage | undefined;
 
-  fileName = '';
-
-  constructor(private http: HttpClient) {}
-
-  onFileSelected(event: Event) {
-    const element = event.currentTarget as HTMLInputElement;
-    let fileList: FileList | null = element.files;
-
-
-    if (fileList != null && fileList[0] != null) {
-
-      const file:File = fileList[0];
-      this.fileName = file.name;
-
-      const formData = new FormData();
-
-      formData.append("thumbnail", file);
-
-      const upload$ = this.http.post("/api/thumbnail-upload", formData);
-
-      upload$.subscribe();
-    }
+  handleImageCapture(webcamImage: WebcamImage) {
+    this.capturedImage = webcamImage;
   }
 
+  handleCameraSwitch(event: any) {
+    // Handle camera switch event if needed
+  }
+
+  handleInitError(error: WebcamInitError) {
+    console.error('Error initializing webcam:', error);
+  }
+
+  onSubmit() {
+    // Handle form submission with the captured image here
+    if (this.capturedImage) {
+      console.log('Captured Image:', this.capturedImage);
+      // You can send the captured image to your server or process it further
+    }
+  }
 }
