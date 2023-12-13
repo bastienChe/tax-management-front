d02e1e198f17ca50391fdabfe239de9893b6148f
Bastien Chevriaut <bastien.chevriaut-ext@pole-emploi.fr>
2023-12-13 14:06:44 +0100

    no more modules since Angular 17

--- tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
+++ tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {MatIconModule} from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-vat-credit-add',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, HttpClientModule],
   templateUrl: './vat-credit-add.component.html',
   styleUrl: './vat-credit-add.component.css'
 })
 export class VatCreditAddComponent {
+
+
+  fileName = '';
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+
+
+    if (fileList != null && fileList[0] != null) {
+
+      const file:File = fileList[0];
+      this.fileName = file.name;
+
+      const formData = new FormData();
+
+      formData.append("thumbnail", file);
+
+      const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+      upload$.subscribe();
+    }
+  }
 
 }
