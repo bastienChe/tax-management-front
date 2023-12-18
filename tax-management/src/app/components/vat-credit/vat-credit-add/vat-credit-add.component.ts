21d2211eb5489dd09abb9090a0b7e4506fc50f03
Bastien Chevriaut <bastien.chevriaut-ext@pole-emploi.fr>
2023-12-18 09:46:09 +0100

    VAT Credit form in bootstrap + error validators

--- tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
+++ tax-management/src/app/components/vat-credit/vat-credit-add/vat-credit-add.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import {MatIconModule} from "@angular/material/icon";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {VatCreditService} from "../vat-credit.service";
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -12,7 +13,7 @@ import {VatCreditService} from "../vat-credit.service";
   imports: [MatIconModule, HttpClientModule, FormsModule, CommonModule, ReactiveFormsModule ],
   templateUrl: './vat-credit-add.component.html',
   styleUrl: './vat-credit-add.component.css',
-  providers: [VatCreditService]
+  providers: [VatCreditService, DatePipe]
 })
 export class VatCreditAddComponent {
   form: FormGroup;
@@ -25,15 +26,20 @@ export class VatCreditAddComponent {
     { name: "TELEPHONE" },
     { name: "AUTRE" }
   ];
+  fromDate: any;
 
-  constructor(private fb: FormBuilder, private vatCreditService: VatCreditService) {   
+  constructor(private fb: FormBuilder, private vatCreditService: VatCreditService, private datePipe: DatePipe) {   
     this.form = this.fb.group({
-      price: [0],
-      date: [""],
-      category: [""],
+      price: [null, Validators.required],
+      date: ["", Validators.required],
+      category: ["", Validators.required],
       categoryExplanation: [""],
-      fileInput: [null]
+      fileInput: [null, Validators.required]
     });
+  }
+
+  ngOnInit() {
+    this.fromDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
   }
 
   onFileChange(event: any): void {
@@ -45,21 +51,58 @@ export class VatCreditAddComponent {
 
   onSubmit() {
     const formData = new FormData();
-    formData.append('price', this.form.value.price);
-    formData.append('date', this.form.value.date);
-    formData.append('category', this.form.value.category);
-    formData.append('categoryExplanation', this.form.value.categoryExplanation);
-    formData.append('file', this.fileUploaded);
 
-    this.vatCreditService.createVatCredit(formData).subscribe(
-      (response) => {
-        console.log('Form data sent successfully:', response);
-        // Handle the response from the server if needed
-      },
-      (error) => {
-        console.error('Error sending form data:', error);
-        // Handle errors if needed
+    if (this.form.valid) { 
+      formData.append('price', this.form.value.price);
+      formData.append('date', this.form.value.date);
+      formData.append('category', this.form.value.category);
+      formData.append('categoryExplanation', this.form.value.categoryExplanation);
+      formData.append('file', this.fileUploaded);
+
+      this.vatCreditService.createVatCredit(formData).subscribe(
+        (response) => {
+          this.vatCreditHttpResultOk(true, "VAT Credit was sent with success.");
+        },
+        (error) => {
+          this.vatCreditHttpResultOk(false, "There was a problem while sending VAT Credit. " + error.message);
+        });
+      } else {
+        document.getElementById('price')?.classList.remove("required-field");
+        document.getElementById('date')?.classList.remove("required-field");
+        document.getElementById('category')?.classList.remove("required-field");
+        document.getElementById('fileInput')?.classList.remove("required-field");
+        if (this.form.value.price == null) document.getElementById('price')?.classList.add("required-field");
+        if (this.form.value.date == "") document.getElementById('date')?.classList.add("required-field");
+        if (this.form.value.category == "") document.getElementById('category')?.classList.add("required-field");
+        if (this.form.value.fileInput == null) document.getElementById('fileInput')?.classList.add("required-field");
+        this.form.markAllAsTouched();
       }
-    );
   }
+
+  vatCreditHttpResultOk(success: boolean, message: string) {
+    const alertElement = document.getElementById('alert');
+    if (alertElement != null) {
+
+      if (success) {
+      alertElement?.classList.remove("alert-danger");
+      alertElement?.classList.add("alert-success");
+      } else {
+        alertElement?.classList.add("alert-danger");
+        alertElement?.classList.remove("alert-success");
+      }
+
+      alertElement?.classList.add("show");
+      alertElement.innerHTML = message;
+      setTimeout(() => {
+        alertElement?.classList.remove("show");
+        alertElement?.classList.add("hide");
+      }, 5000);
+
+      this.form.value.price = null;
+      this.form.value.category = null;
+      this.form.value.fileInput = null;
+      this.form.value.categoryExplanation = null;
+    }
+  }
+
 }
